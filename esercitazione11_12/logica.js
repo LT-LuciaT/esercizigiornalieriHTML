54beb5ce57ae51a9a2da17c278da4d152beb7e30
LT-LuciaT <lucia.trombin98@gmail.com>
2024-12-11 18:03:19 +0100

    COMMIT

--- esercitazione11_12/logica.js
+++ esercitazione11_12/logica.js
@@ -6,29 +6,36 @@ function addTask() {
     const taskText = taskInput.value.trim();
     if (taskText === '') return;
 
-const li = document.createElement('li');
-li.classList.add('task');
+    const li = document.createElement('li');
+    li.classList.add('task');
 
-const span = document.createElement('span');
-span.textContent = taskText;
+    const span = document.createElement('span');
+    span.textContent = taskText;
+    span.addEventListener('click', function(e){
+     //span.style.textDecoration = 'lineThrough'
+      // console.log(e.target) 
+       e.target.style.textDecoration = 'line-through'
+       e.target.style.color = 'red'
+    })
 
-const deleteButton = document.createElement('button');
-deleteButton.textContent = 'Elimina';
-deleteButton.onclick = function () {
-    taskList.removeChild(li);
-}
-li.onclick = function () {
-  li.classList.toggle('completato');
-}
-li.appendChild(span);
-li.appendChild(deleteButton);
-taskList.appendChild(li)
-taskInput.value = '';
+    const deleteButton = document.createElement('button');
+
+    deleteButton.style.float = 'right'
+    deleteButton.textContent = 'Elimina';
+    deleteButton.addEventListener('click', function () {
+        taskList.removeChild(li);
+    })  
+    
+
+    li.appendChild(span);
+    li.appendChild(deleteButton);
+    taskList.appendChild(li)
+    taskInput.value = '';
 }
 addButton.addEventListener('click', addTask);
+
 taskInput.addEventListener('keypress', function (event){
-    if (event.key === 'enter') {
-        addTask();
-        
+    if (event.key === 'Enter') {
+        addTask(); 
     }
 });
